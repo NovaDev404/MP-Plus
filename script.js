9e72fed1c9487ba3df80b396d36ef1143be7cba1
Qualliminuim <AmbroseDa@students.kbgs.sa.edu.au>
2025-11-19 15:19:11 +1030

    Update script.js

--- script.js
+++ script.js
@@ -81,7 +81,7 @@
             emojis: ['𓇼','🌊','🐚','🫧','🪼','⛱️','🏝️','🏄','🌴','🍹','🌞', 'OCEAN VIBES']
         },
          space: {
-            gradient: 'linear-gradient(black, black, white, black, black, white, black, black, white, black, black, white, black, black)',
+            gradient: 'linear-gradient(black, white, black, lightgray, black, black, gray, black, black, white, black, black, gray, black, black)',
             emojis: ['✦', '⭑', '🛸', '˚', '☆', '⋆', '🛰', '⭑', '🌎', '✦', '✦', '⭑', '🛸', '⭑', 'SPACE']
          }
     };
